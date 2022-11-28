20d0fe454d89ac440c338c68ba3c5fb63ad841d9
Will Feng (DPER) <willfeng@fb.com>
2022-11-28 15:13:23 -0800

    update

--- meshroom/core/__init__.py
+++ meshroom/core/__init__.py
@@ -1,5 +1,3 @@
-from __future__ import print_function
-
 import hashlib
 from contextlib import contextmanager
 import importlib
--- meshroom/nodes/aliceVision/DepthMapImport.py
+++ meshroom/nodes/aliceVision/DepthMapImport.py
@@ -1,3 +1,5 @@
+__version__ = "0.1"
+
 from meshroom.core import desc
 import os
 import cv2 as cv
