1c8f44ca5fcd5083f61507843f1f767bdf64c0ff
Will Feng (DPER) <willfeng@fb.com>
2022-11-29 16:17:33 -0800

    update

--- meshroom/nodes/aliceVision/DepthMapImport.py
+++ meshroom/nodes/aliceVision/DepthMapImport.py
@@ -125,11 +125,17 @@ That script expect the depth image to be aside the rgb image, and have similar n
     def importDepthMaps(self, chunk, cameras, inputDepthMapsFolder, outputDepthMapsFolder, depthIntrinsics, rgbIntrinsics, rgbImageSuffix, depthImageSuffix):  #, ratio = 0.0):
         chunk.logger.info(f"depthIntrinsics: {depthIntrinsics}")
         chunk.logger.info(f"rgbIntrinsics._objects: {rgbIntrinsics._objects}")
-        for key in rgbIntrinsics._objects[0]._value._objects:
-            chunk.logger.info(f"key, rgbIntrinsics._objects[0]._value._objects: {key},{rgbIntrinsics._objects[0]._value._objects[key]}")
-        import pickle
-        with open('/fsx/users/willfeng/3d_recon/Meshroom/rgbIntrinsics.pickle', 'wb') as f:
-            pickle.dump(rgbIntrinsics._objects[0]._value._objects, f, protocol=pickle.HIGHEST_PROTOCOL)
+        import meshroom
+        for key, obj in rgbIntrinsics._objects[0]._value._objects:
+            if type(obj) == meshroom.core.attribute.Attribute:
+                chunk.logger.info(f"{key}, {obj._value}")
+            elif type(obj) == meshroom.core.attribute.GroupAttribute:
+                chunk.logger.info(f"{key}, {obj._value._objects}")
+            elif type(obj) == meshroom.core.attribute.ListAttribute:
+                chunk.logger.info(f"{key}, {obj._value._objects}")
+        # import pickle
+        # with open('/fsx/users/willfeng/3d_recon/Meshroom/rgbIntrinsics.pickle', 'wb') as f:
+        #     pickle.dump(rgbIntrinsics._objects[0]._value._objects, f, protocol=pickle.HIGHEST_PROTOCOL)
 
         f = open(cameras,)
         data = json.load(f)
